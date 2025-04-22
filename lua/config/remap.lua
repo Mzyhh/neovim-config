7fa473f0db81e748b64beee074adfed60dbedf2b
Grigory Sedov <xiiad@mail.ru>
2025-04-23 01:46:12 +0300

    Delete extra mappings

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -7,9 +7,5 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("i", "jk", "<Esc>")
 
-vim.keymap.set("i", "{<CR>", "{<CR>}<Esc>k$a<CR>")
-vim.keymap.set("i", "[<CR>", "[<CR>]<Esc>k$a<CR>")
-vim.keymap.set("i", "(<CR>", "(<CR>)<Esc>k$a<CR>")
-
 -- clear highlights after searching
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
